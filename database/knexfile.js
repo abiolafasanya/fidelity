9f1fe7a3708de44d38c94368ace905a1b7ee0b08
fastbeetech <hezekiahfasanya@gmail.com>
2021-12-11 21:44:13 +0100

    update

--- database/knexfile.js
+++ database/knexfile.js
@@ -52,8 +52,9 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: DATABASE_URL,
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
     pool: {
       min: 2,
       max: 10,
