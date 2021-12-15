1f9d1b78cc297b29a125e51ebd785ef3643ec4c0
fastbeetech <79338077+fastbeetech@users.noreply.github.com>
2021-12-15 22:22:34 +0100

    update

--- utils/db.js
+++ utils/db.js
@@ -1,29 +1,29 @@
-let sqlite3 = require("sqlite3");
-let dbname = "./db/fidelity.sqlite3";
+// let sqlite3 = require("sqlite3");
+// let dbname = "./db/fidelity.sqlite3";
 
-const dbsqlite = require("knex")({
-  client: "sqlite3",
-  connection: {
-    filename: dbname,
-  },
-});
+// const dbsqlite = require("knex")({
+//   client: "sqlite3",
+//   connection: {
+//     filename: dbname,
+//   },
+// });
 
-const connection = {
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  port: 3306,
-  database: "fidelity",
-};
+// const connection = {
+//   host: "127.0.0.1",
+//   user: "root",
+//   password: "",
+//   port: 3306,
+//   database: "fidelity",
+// };
 
-const migrations = {
-  tableName: 'knex_migration'
-}
+// const migrations = {
+//   tableName: 'knex_migration'
+// }
 
-const dbmysql = require("knex")({
-  client: "mysql",
-  connection,
-  migrations
-});
+// const dbmysql = require("knex")({
+//   client: "mysql",
+//   connection,
+//   migrations
+// });
 
-module.exports = { dbmysql, dbsqlite };
+// module.exports = { dbmysql, dbsqlite };
